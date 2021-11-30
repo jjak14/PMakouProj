c6d447cdd59106035419f2c1bb84ed6079e9d329
jj@k <jakam.jurgen@gmail.com>
2021-11-30 09:43:53 -0600

    second commit

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'cicool',
+	'hostname' => 'sql-server.k8s-966qdrrl',
+	'username' => 'worldtroc',
+	'password' => '	}KBX=YCueP',
+	'database' => 'worldtroc',
 	'port' 	   => '3306',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
